c8370dfc507be7475bdea8290e2745b68ea292fa
C. Asu <100544268+Clarence-asu@users.noreply.github.com>
2026-04-02 09:42:10 -0400

    deploy.sh

--- Part-2-App-Container-Setup/scripts/deploy.sh
+++ Part-2-App-Container-Setup/scripts/deploy.sh
@@ -3,3 +3,15 @@ LB_SG_ID=$(aws cloudformation list-exports \
   --output text)
 
 sed -i "s/LB_SG_PLACEHOLDER/$LB_SG_ID/" service_lb_manifest.yaml
+
+ECR_URI=$(aws cloudformation list-exports \
+  --query "Exports[?Name=='mtier:ErcRepository'].Value" \
+  --output text)
+
+sed -i "s/ECR_URI_PLACEHOLDER/$ECR_URI/" deployment_manifest.yaml
+
+
+kubectl apply -f namespace_manifest.yaml
+kubectl apply -f configmap_manifest.yaml
+kubectl apply -f deployment_manifest.yaml
+kubectl apply -f service_lb_manifest.yaml
