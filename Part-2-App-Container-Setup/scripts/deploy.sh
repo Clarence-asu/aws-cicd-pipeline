ff78e309598dfc3c768994cd8347bed38f27b1f3
Clarence-asu <clarenceasu@gmail.com>
2026-04-18 14:47:54 -0400

    Fix deploy.sh manifest file paths

--- Part-2-App-Container-Setup/scripts/deploy.sh
+++ Part-2-App-Container-Setup/scripts/deploy.sh
@@ -2,16 +2,15 @@ LB_SG_ID=$(aws cloudformation list-exports \
   --query "Exports[?Name=='mtier:LoadBalancerSGId'].Value" \
   --output text)
 
-sed -i "s/LB_SG_PLACEHOLDER/$LB_SG_ID/" service_lb_manifest.yaml
+sed -i "s/LB_SG_PLACEHOLDER/$LB_SG_ID/" ../manifests/service_lb_manifest.yaml
 
 ECR_URI=$(aws cloudformation list-exports \
   --query "Exports[?Name=='mtier:ErcRepository'].Value" \
   --output text)
 
-sed -i "s/ECR_URI_PLACEHOLDER/$ECR_URI/" deployment_manifest.yaml
+sed -i "s/ECR_URI_PLACEHOLDER/$ECR_URI/" ../manifests/deployment_manifest.yaml
 
-
-kubectl apply -f namespace_manifest.yaml
-kubectl apply -f configmap_manifest.yaml
-kubectl apply -f deployment_manifest.yaml
-kubectl apply -f service_lb_manifest.yaml
+kubectl apply -f ../manifests/namespace_manifest.yaml
+kubectl apply -f ../manifests/configmap_manifest.yaml
+kubectl apply -f ../manifests/deployment_manifest.yaml
+kubectl apply -f ../manifests/service_lb_manifest.yaml
